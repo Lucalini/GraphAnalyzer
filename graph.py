4411c3f3194a39bba7b453f2e76c86029cb535fa
Luca Verweyen <109435124+Lucalini@users.noreply.github.com>
2024-01-08 20:00:14 -0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,9 +2,7 @@ from stack_array import * #Needed for Depth First Search
 from queue_array import * #Needed for Breadth First Search
 
 class Vertex:
-    '''Add additional helper methods if necessary.'''
     def __init__(self, key):
-        '''Add other Attributes as necessary'''
         self.id = key
         self.adjacent_to = []
         self.color = None
@@ -12,14 +10,7 @@ class Vertex:
 
 
 class Graph:
-    '''Add additional helper methods if necessary.'''
     def __init__(self, filename):
-        '''reads in the specification of a graph and creates a graph using an adjacency list representation.  
-           You may assume the graph is not empty and is a correct specification.  E.g. each edge is 
-           represented by a pair of vertices.  Note that the graph is not directed so each edge specified 
-           in the input file should appear on the adjacency list of each vertex of the two vertices associated 
-           with the edge.'''
-        # This method should call add_vertex and add_edge!!!
         self.graph = {}
 
         f = open(filename)
@@ -37,34 +28,24 @@ class Graph:
 
     def add_vertex(self, key):
         # Should be called by init
-        '''Add vertex to graph only if the vertex is not already in the graph.'''
         if key not in self.graph:
             self.graph.update({key: Vertex(key)})
 
     def add_edge(self, v1, v2):
         # Should be called by init
-        '''v1 and v2 are vertex ID's. As this is an undirected graph, add an 
-           edge from v1 to v2 and an edge from v2 to v1.  You can assume that
-           v1 and v2 are already in the graph'''
         self.graph[v1].adjacent_to.append(v2)
         self.graph[v2].adjacent_to.append(v1)
 
     def get_vertex(self, key):
-        '''Return the Vertex object associated with the ID. If ID is not in the graph, return None'''
         if key in self.graph:
             return self.graph[key]
         else:
             return None
     def get_vertices(self):
-        '''Returns a list of ID's representing the vertices in the graph, in ascending order'''
         vertices_list = sorted(list(self.graph))
         return vertices_list
 
     def conn_components(self): 
-        '''Return a Python list of lists.  For example: if there are three connected components 
-           then you will return a list of three lists.  Each sub list will contain the 
-           vertices (in ascending alphabetical order) in the connected component represented by that list.
-           The overall list will also be in ascending alphabetical order based on the first item in each sublist.'''
         #This method MUST use Depth First Search logic!
         keys = self.graph.keys()
         for key in keys:
@@ -94,7 +75,6 @@ class Graph:
         return componenets_list
 
     def is_bipartite(self):
-        '''Return True if the graph is bipartite, False otherwise.'''
         #This method MUST use Breadth First Search logic!
 
         keys = self.graph.keys()
