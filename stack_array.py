0faa6a272c38677a9fce7e0aaeffd5e4d0a16b7f
Luca Verweyen <109435124+Lucalini@users.noreply.github.com>
2024-01-08 19:57:45 -0800

    Update stack_array.py

--- stack_array.py
+++ stack_array.py
@@ -1,32 +1,28 @@
 class Stack:
-    '''Implements an efficient last-in first-out Abstract Data Type using a Python List'''
 
     def __init__(self, capacity):
-        '''Creates and empty stack with a capacity'''
+        #Creates and empty stack with a capacity
         self.capacity = capacity
         self.items = [None]*capacity
         self.num_items = 0 
 
     def is_empty(self):
-        '''Returns True if the stack is empty, and False otherwise
-           MUST have O(1) performance'''
+        #Returns True if the stack is empty, and False otherwise
         if self.num_items == 0:
             return True
         else:
             return False
 
     def is_full(self):
-        '''Returns True if the stack is full, and False otherwise
-           MUST have O(1) performance'''
+        #Returns True if the stack is full, and False otherwise
         if self.num_items < self.capacity:
             return False
         else:
             return True
 
     def push(self, item):
-        '''If stack is not full, pushes item on stack. 
-           If stack is full when push is attempted, raises IndexError
-           MUST have O(1) performance'''
+        #If stack is not full, pushes item on stack. 
+        #If stack is full when push is attempted, raises IndexError
         if self.is_full() == True:
             raise IndexError
         else:
@@ -34,9 +30,8 @@ class Stack:
             self.num_items += 1
 
     def pop(self): 
-        '''If stack is not empty, pops item from stack and returns item.
-           If stack is empty when pop is attempted, raises IndexError
-           MUST have O(1) performance'''
+        #If stack is not empty, pops item from stack and returns item.
+        #If stack is empty when pop is attempted, raises IndexError
         if self.is_empty():
             raise IndexError
         else:
@@ -46,15 +41,12 @@ class Stack:
             return return_val
 
     def peek(self):
-        '''If stack is not empty, returns next item to be popped (but does not pop the item)
-           If stack is empty, raises IndexError
-           MUST have O(1) performance'''
+        #If stack is not empty, returns next item to be popped (but does not pop the item)
+        #If stack is empty, raises IndexError
         if self.is_empty():
             raise IndexError
         else:
             return self.items[self.num_items -1]
 
     def size(self):
-        '''Returns the number of elements currently in the stack, not the capacity
-           MUST have O(1) performance'''
         return self.num_items
